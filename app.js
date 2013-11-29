56d6054706a9a4d877b53c52688a610f4bf31deb
raphael-amar <raphael.amar@live.fr>
2013-11-29 13:53:04 +0300

    rajout d'un node_module generic-parser avec parser factory et début d'implémentation de ces parseurs. envoi de fichier depuis une application cliente en post et recuperation des donnees côté serveur dans app.js à la racine du projet

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 var colors = require('colors');
 var sage = require("sage");
 var esi = sage('http://localhost:9200/batiment');
+var fs = require('fs');
+var parserFactory = require('generic-parser');
+
 
 esi.create(function(err, result) {
     if (result.error)
@@ -16,7 +19,44 @@ var est = esi.type('service-public');
 app.configure(function () {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.bodyParser({ keepExtensions: true}));
     app.use(express.static(__dirname + '/public'));
+});
+
+app.use(function(req, res, next){
+  if (req.is('text/*') || req.is('application/*')) {
+    req.text = '';
+    //req.setEncoding('utf8');
+    req.on('data', function(chunk){ req.text += chunk });
+    console.log(req.text);
+    req.on('end', next);
+  } else {
+    next();
+  }
+});
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+/*var parserFactoryInstance = new parserFactory("test.csv", "csv");
+parserFactoryInstance.createParser();*/
+
+app.post('/upload', function(req, res) {
+   var ext = req.headers['content-type'].split(';')[0].split('/')[0];
+    /*if (req.body.length <= 2 && req.text.length <= 2)
+        return;*/
+ fs.writeFile('test.xls', req.text, function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
+    console.log(req.text);
+  console.log(req.headers['content-type'].split(';')[0].split('/')[0]);
+ /* var parserFactoryInstance = new parserFactory(req.body.length > 2 ? req.body : req.text, ext);*/
+  console.log(parserFactory);
 });
 
 app.configure('development', function () {
