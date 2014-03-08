1d9ea228c75a6331f69b44edca3344b394aa0145
raphael-amar <raphael.amar@live.fr>
2014-03-09 01:18:32 +0300

    Rajout de la documentation pour le module genericParser. Modification des tests unitaires et test d'un cas d'erreur pour le fichier csv. Mise a jour de la documentation du module genericParser et intégration de cette doc dans la documentation générale

--- routes/sources.js
+++ routes/sources.js
@@ -110,9 +110,11 @@ exports.getModel = function(req, res) {
         index: 'sources'
     }, function(error, response, status) {
         var category = req.params.category;
-        if (!req.query || !category || !response 
-            || !response.sources || !response.sources[category] 
-            || !response.sources[category].properties) {
+        console.log(response.sources[category]);
+        
+        if (!category || !response || !response.sources
+            || !response.sources.mappings || !response.sources.mappings[category] 
+            || !response.sources.mappings[category].properties) {
                 res.json(200, {
                     status: "error",
                     message: "from: " + req.url + ": No mapping available. Maybe it's because there is no source uploaded in elasticsearch ?"
