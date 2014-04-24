ad6a1189707f6c31bb01840450e84b2a91629495
Cyril Moralès <cyril.morales@epitech.eu>
2014-04-24 17:26:17 +0300

    Ajout de toutes les routes manquantes

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,10 @@ var middleware = require('./middleware');
 var uploadDir = "./uploads/";
 
 /*
- * User middleware
+ * User and file middleware
  */
 router.param('publicKey', middleware.publicKey);
+router.param('path', middleware.path);
 
 
 /*
@@ -99,5 +100,97 @@ router.post('/:publicKey/files/add', function(req, res) {
 	});
 	form.parse(req);
 });
+
+/*
+ * GET the list of files of the user
+ */
+router.get('/:publicKey/files/list', function(req, res) {
+    var db = req.db;
+	var publicKey = req.params.publicKey;
+
+	db.search({
+		index: 'files',
+		type: 'file',
+		body: {
+			query: {
+				match: {
+					publicKey: publicKey
+				}
+			}
+		}
+	}).then(function (resp) {
+		var list = [];
+		for (var file in resp.hits.hits) {
+			list.push(resp.hits.hits[file]["_source"]);
+		}
+		res.json(200, {
+			status: "success",
+			data: list
+		});
+	}, function (err) {
+		console.trace(err.message);
+		return next(err);
+	});
+});
+
+/*
+ * GET parse the file with geenericparser
+ */
+router.get('/:publicKey/files/:path/parse', function(req, res, next) {
+	var db = req.db;
+	var dirName = uploadDir + path;
+	
+	var name = req.file.name;
+	var typeTab = name.split('.');
+	var type = typeTab[typeTab.length - 1].toLowerCase();
+	var parser = genericParser(type);
+	if (!parser) {
+		return next(new Error("the file [" + name + "] can't be parsed. Incompatible file type."));
+	}
+	parser.on("error", function (err) {
+		return next(new Error(err));
+	});
+	parser.parse(dirName, false, function (result, index) {
+		if (result)
+			res.json(200, {
+				status: "success",
+				data: result
+			});
+	});
+});
+
+
+/*
+ * delete a file of an user
+ */
+router.delete('/:publicKey/files/:path', function(req, res, next) {
+    var db = req.db;
+	var path = req.params.path;
+	var dirName = uploadDir + path;
+
+	db.deleteByQuery({
+		index: 'users',
+		type: 'user',
+		q: 'path:"' + path + '"'
+	}).then(function (resp) {
+		fs.unlink(dirName, function (err) {
+			if (err) {
+				return next(new Error("Unable to delete the file on the file system"));
+			}
+			res.json(200, {
+				status: "success",
+				data: "user deleted"
+			});
+		});
+	}, function (err) {
+		console.trace(err.message);
+		return (next(err));
+	});
+});
+
+
+
+
+
 
 module.exports = router;
